43d3b8520bb4a2997d50ebfc3b622968eaeaa861
Chenhe <cg3293@nyu.edu>
2021-12-08 01:07:25 +0800

    added few more posts, changed post height

--- insta-feed/src/App.js
+++ insta-feed/src/App.js
@@ -3,8 +3,8 @@ import Post from './components/Post';
 import Header from './components/Header';
 import { Component } from 'react/cjs/react.production.min';
 
-const postImgs = ['post1. jpg', 'post1.jpg'];
-const captions = ['kekeke', 'yeet'];
+const postImgs = ['post1.jpg', 'post2.jpg', 'post3.jpg', 'post4.jpg', 'post5.jpg'];
+const captions = ['kekeke', 'yeet', 'adfadsfasdf', 'aesthetic', 'asdfas'];
 
 const posts = [];
 for (let [index, value] of postImgs.entries()) {
@@ -24,7 +24,7 @@ class App extends Component {
         <div>
 
           {posts}
-          
+
         </div>
 
       </div>
